f82432b8a185d4953c6711e0de4afc2b4c24ee0a
arthusfw <arthusfw@icloud.com>
2022-11-20 17:37:01 +0100

    fct att

--- main.c
+++ main.c
@@ -4,3 +4,11 @@ int main()
 {
     printf("hello world");
 }
+
+void attente(int temps) // attend temps secondes/1000
+{
+    clock_t hdeb;
+    hdeb=clock();
+    while (clock()-hdeb<=temps*CLOCKS_PER_SEC/1000);
+    return;
+}
